39c295b775c33000420f44dd6486b85cdee487b5
lingit <lingit@protonmail.com>
2026-07-31 11:55:01 +0300

    Update protocol.py

--- src/cleint/protocol.py
+++ src/cleint/protocol.py
@@ -1,27 +1,17 @@
 import json
 import uuid
-from datetime import datetime
-from config import PROTOCOL_VERSION
+import config
 
-def build_packet(msg_type: str, payload: str, to: str, fr: str) -> str:
+def build_packet(msg_type: str, payload: str, to: str = "SRV") -> str:
     raw_packet = {
         "msg-id": f"msg-{uuid.uuid4().hex[:8]}",
         "type": msg_type,
-        "fr": fr,
         "to": to,
         "pl": payload,
-        "tm": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "v": PROTOCOL_VERSION
+        "v": config.PROTOCOL_VERSION
     }
     packet = {key: value for key, value in raw_packet.items() if value is not None}
     return json.dumps(packet, ensure_ascii=False) + "\n"
 
-def send_packet(sock, msg_type, payload="", to="SRV", fr: str = ""):
-    packet_str = build_packet(msg_type, payload, to, fr)
-    try:
-        sock.sendall(packet_str.encode("utf-8"))
-    except OSError:
-        return
-    date_and_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open("logs/server.log", "a", encoding="utf-8") as f:
-        f.write(f"[{date_and_time}] [OUT] {packet_str}")
+def send_packet(sock, msg_type, payload="", to="SRV"):
+    sock.sendall(build_packet(msg_type, payload, to).encode("utf-8"))
